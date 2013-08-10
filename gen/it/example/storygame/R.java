5ded2168a0adc015d4a437361bb9b5932af4a077
emulk <orgest.shehaj@gmail.com>
2013-08-10 01:19:00 +0100

    add button style

--- gen/it/example/storygame/R.java
+++ gen/it/example/storygame/R.java
@@ -163,11 +163,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int icon16=0x7f020001;
-        public static final int icon32=0x7f020002;
-        public static final int icon48=0x7f020003;
-        public static final int icon72=0x7f020004;
+        public static final int bluebutton=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon16=0x7f020002;
+        public static final int icon32=0x7f020003;
+        public static final int icon48=0x7f020004;
+        public static final int icon72=0x7f020005;
+        public static final int redbutton=0x7f020006;
     }
     public static final class id {
         public static final int Indietroread=0x7f090009;
@@ -247,5 +249,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int button_text=0x7f070002;
     }
 }
