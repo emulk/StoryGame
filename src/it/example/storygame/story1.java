38e05cfeecdec8526ede267f8b76b4c82d35e161
emulk <orgest.shehaj@gmail.com>
2013-09-29 17:42:22 +0200

    Update story1.java

--- src/it/example/storygame/story1.java
+++ src/it/example/storygame/story1.java
@@ -67,15 +67,15 @@ public class story1 extends Activity {
 		//textview.setMovementMethod(new ScrollingMovementMethod()); 
 		textview.setMovementMethod(new ScrollingMovementMethod());
 		textview.setText("Questo è un giorno come tutti gli altri nelle Terre Tormentate. Sono passati 4 mesi ormai da quando \"I Signori delle Tenebre\" hanno dichiarato "
-		+"guerra al mio paese, le prime luci dell'alba iniziano a intravedersi all'orizonte e tra poco la grande campana d'oro inizierà a emettere un "
-		+"suono assordante come fa ogni mattina e indicherà ad ogni guerriero di questo paese che sara' tempo di addestramento."
-		+"Sono passati tre anni da quando sono entrato a fare parte dell' esercito reale, e sono stato promosso come \"Cavaliere\" delle Terre Tormentate "
-		+"quando riuscii a dimostrare il mio coraggio nelle tre Arti della Guerra che ogni uomo dovrebbe dimostrare per avere l'onore di farsi "
+		+"guerra al mio paese. Le prime luci dell'alba iniziano a intravedersi all'orizzonte e tra poco la grande campana d'oro inizierà a emettere un "
+		+"suono assordante come fa ogni mattina e indicherà ad ogni guerriero di questo paese che è tempo di addestramento."
+		+"Sono passati tre anni da quando sono entrato a fare parte dell' esercito reale, e sono stato promosso come \"Cavaliere\" delle Terre Tormentate, "
+		+"quando riuscii a dimostrare, come ogni uomo dovrebbe fare, il mio coraggio nelle tre Arti della Guerra per avere l'onore di farsi "
 		+"chiamare \"Guerriero\". Ho imparato l'arte della guerra che molti riconoscono con il nome di \"controllo animale\" grazie alla mia amatissima "
-		+"madre, che era l'alchimista piu' conosciuta e rispettata di tutto il villaggio, ora ogni volta che mi alleno sono capace di comunicare "
-		+"con molti animali i quali mi informano delle posizioni dei miei nemici e mi danno dei preziosissimi consigli. Ed è stato sempre grazie a " 
+		+"madre che era l'alchimista piu' conosciuta e rispettata di tutto il villaggio. Ora, ogni volta che mi alleno sono capace di comunicare "
+		+"con molti animali che mi informano delle posizioni dei nemici e mi danno dei preziosissimi consigli. Ed è stato sempre grazie a " 
 		+"lei, che all'eta di 7 anni mi ha  insegnato l'arte della \"Divinazione\", che ora riesco a capire le vere intenzioni della persona che ho di "
-		+"fronte e riesco a comunicare telepaticamente con ogni creatura che e' dotata di capacità psichiche. E per il mio dodicessimo compleanno " 
+		+"fronte e riesco a comunicare telepaticamente con ogni creatura che e' dotata di capacità psichiche. E per il mio dodicesimo compleanno " 
 		+"la mia amata madre mi insegno' l'arte della \"Sparizione\".... Questa tecnica mi permette di adattarmi all'ambiente circostante e mi rende capace "
 		+"di mascherare l'odore e il calore del mio corpo rendendomi invisibile agli occhi di tutti.");
 				
@@ -101,10 +101,10 @@ public class story1 extends Activity {
 				textview = (TextView) findViewById(R.id.story1);
 				textview.setText("Mi alzo in piedi per preparare la mia borsa e partire come ogni giorno ormai da tre lunghissimi anni per il" +
 						" campo d'addestramento. Guardo fuori dalla finestra e l'atmosfera è stranamente silenziosa, la brina del mattino luccica " +
-						"sui merli delle torri, l'aria e pungente. " );
+						"sui merli delle torri, l'aria è pungente. " );
 				
 				//1 prima scelta
-				avanti.setText("Ritorna a dorimire");
+				avanti.setText("Ritorna a dormire");
 				avanti.setOnClickListener(new View.OnClickListener() {
 						
 						@Override
@@ -114,9 +114,9 @@ public class story1 extends Activity {
 							
 							textview = (TextView) findViewById(R.id.story1);
 							textview.setText("Dopo tre ore di sonno vengo svegliato dal rumore assordante che la porta produce cadendo per terra e " +
-									"frantumandosi in mille pezzi.In controluce appare la faccia di un assasino, era li per derubarmi." +
-									" A 40 centimetri alla destra del letto c'era la finestra aperta che dava sul lago, mentre sopra la sedia " +
-									"sulla mia sinistra lucicava l'argento della spada che avevo ereditato dal mio defunto padre. " );
+									"frantumandosi in mille pezzi. In controluce appare la faccia di un assassino, era li per derubarmi." +
+									" A 40 centimetri alla destra del letto c'era la finestra aperta che da sul lago, mentre sopra la sedia " +
+									"sulla mia sinistra luccicava l'argento della spada che avevo ereditato dal mio defunto padre. " );
 							
 							avanti.setText("Buttati dalla finestra!");
 							avanti.setOnClickListener(new View.OnClickListener() {
@@ -130,15 +130,15 @@ public class story1 extends Activity {
 									
 									textview = (TextView) findViewById(R.id.story1);
 									textview.setText("Mi alzo di colpo e mi lancio nel vuoto attraverso la finestra. Cado per tre metri e cado in piedi " +
-											"per poi sbattere violentemente il 	ginochio destro per terra e procurarmi una ferita che inizio a" +
-											" perdere sangue. Mi rialzo, senza pensare alla ferita e inizio a correre 	all'impazata, l'assasino mi" +
-											" corre dietro con un coltello in mano. All'orizonte vedo una guardia reale in servizio e inizio a gridare " +
-											"con tutto il fiato che mi era rimasto. La guardia si acorge di me e inizia a corrermi incontro, tira fuori" +
-											" il suo arco e scoca una freccia con la quale trapassa il cranio del assasino. Io vengo ricorso e" +
-											" tranquillizato ma poche ore dopo vengo ragiunto dal mio comandante il quale mi da la brutta notizia che" +
-											" a causa del mio disonore per la cavalleria ero stato escluso dal esercito. Torno a casa e inizio a pensare " +
-											" al disonore che avevo causato alla mia famigli. Mi rendo conto che tutto quell'disonore era troppo " +
-											"grande da soportare, prendo una corda e decido di impicarmi.");
+											"per poi sbattere violentemente il ginochio destro per terra e procurarmi una ferita che iniziò a" +
+											" sanguinare. Mi rialzo, senza pensare alla ferita e inizio a correre all'impazata, l'assasino mi" +
+											" corre dietro con un coltello in mano. All'orizzonte vedo una guardia reale in servizio e inizio a gridare " +
+											"con tutto il fiato che mi era rimasto. La guardia si accorge di me e inizia a corrermi incontro, tira fuori" +
+											" il suo arco e scocca una freccia con la quale trapassa il cranio dell'assassino. Io vengo ricorso e" +
+											" tranquillizzato ma poche ore dopo vengo raggiunto dal mio comandante il quale mi da la brutta notizia che" +
+											" a causa del mio disonore per la cavalleria ero stato escluso dall'esercito. Torno a casa e inizio a pensare " +
+											" al disonore che avevo causato alla mia famiglia. Mi rendo conto che tutto quel disonore era troppo " +
+											"grande da sopportare, prendo una corda e decido di impiccarmi.");
 									
 									//obbiettivo 2: morire almeno una volta
 									theIntent.putExtra("score", 2);
@@ -161,7 +161,7 @@ public class story1 extends Activity {
 							});
 							
 							second.setVisibility(View.VISIBLE);
-							second.setText("Affera la spada!");
+							second.setText("Afferra la spada!");
 							second.setOnClickListener(new View.OnClickListener() {
 								
 								@Override
@@ -169,19 +169,19 @@ public class story1 extends Activity {
 									// TODO Auto-generated method stub
 									
 									textview = (TextView) findViewById(R.id.story1);
-									textview.setText("Affero la spada e tiro un primo fendente all'assasino staccandogli la testa dal corpo, il sangue schizza" +
-											" da per tutto e quattro corone d'oro cadono dalle tasche del assasino rotolando per terra.");
+									textview.setText("Affero la spada e tiro un primo fendente all'assassino staccandogli la testa dal corpo, il sangue schizza" +
+											" dappertutto e quattro corone d'oro cadono dalle tasche dell'assasino rotolando per terra.");
 									//obbiettivo 3: trova la spada
 									theIntent.putExtra("score", 3); 
 									startActivity(theIntent);
-								avanti.setText("Sbarazati del corpo!");
+								avanti.setText("Sbarazzati del corpo!");
 								avanti.setOnClickListener(new View.OnClickListener() {
 									
 									@Override
 									public void onClick(View v) {
 										
 										textview = (TextView)findViewById(R.id.story1);
-										textview.setText("Scendo nella stalla e afferro un sacco, all' interno del quale ci inserisco il cadavere del assassino.");
+										textview.setText("Scendo nella stalla e afferro un sacco, all' interno del quale ci inserisco il cadavere dell'assassino.");
 										
 										
 										avanti.setText("Butta il cadavere nel lago!");
@@ -193,7 +193,7 @@ public class story1 extends Activity {
 												
 												textview=(TextView)findViewById(R.id.story1);
 												textview.setText("Carico il cadavere sulle spalle e lo porto fino alla riva del lago per poi buttarlo all' interno. " +
-														"Rimango in piedi a fissarlo per più di 10 minuti dopo di che il sacco sparisce sott'acqua e decido di ritornare nell' appartamento.");
+														"Rimango in piedi a fissarlo per più di 10 minuti finchè il sacco sparisce sott'acqua e decido di ritornare nell'appartamento.");
 												
 												avanti.setText("Pulisci la stanza!");
 												avanti.setOnClickListener(new View.OnClickListener() {
@@ -204,11 +204,11 @@ public class story1 extends Activity {
 														textview=(TextView)findViewById(R.id.story1);
 														textview.setText("Scendo al lago sottostante e riempio un barilotto con l'acqua limpida" +
 																" che si trova al suo interno, ritono nella mia camera e inizio a pulire il sangue che aveva macchiato ogni cosa." +
-																" Una volta finito la stanza splendeva come non mai, ma davanti alla porta giace per terra il cadavere freddo e" +
-																" senza vita del assasino e come se non bastase sono di parecchie ore in ritardo per l'adestramento.");
+																" Una volta finito la stanza splendeva come non mai, ma davanti alla porta giaceva per terra il cadavere freddo e" +
+																" senza vita dell'assassino e come se non bastasse sono di parecchie ore in ritardo per l'addestramento.");
 														
 														
-														second.setText("Parti per l'adestramento!");
+														second.setText("Parti per l'addestramento!");
 														second.setOnClickListener(new View.OnClickListener() {
 															
 															@Override
@@ -222,7 +222,7 @@ public class story1 extends Activity {
 												});
 												
 												avanti.setVisibility(View.INVISIBLE);
-												second.setText("Parti per l'adestramento!");
+												second.setText("Parti per l'addestramento!");
 												theIntent.putExtra("score", 4);
 												startActivity(theIntent);
 												second.setOnClickListener(new View.OnClickListener() {
@@ -239,7 +239,7 @@ public class story1 extends Activity {
 											}
 										});
 										
-										second.setText("Porta il cadavere nel bosco per sotterarlo!");
+										second.setText("Porta il cadavere nel bosco per sotterrarlo!");
 										second.setOnClickListener(new View.OnClickListener() {
 											
 											@Override
@@ -248,12 +248,12 @@ public class story1 extends Activity {
 												textview=(TextView)findViewById(R.id.story1);
 												textview.setText("Afferro il sacco con forza e lo carico sopra il cavallo, salgo in groppa e parto in direzzione del bosco." +
 														" Scendo a cavallo per il ripido sentiero che si inoltra nella foresta di Freylund, mi inoltro al suo interno e " +
-														" in prossimita di un dirupo mi fermo per poi butare al suo interno il sacco");
+														" in prossimità di un dirupo mi fermo per poi buttare al suo interno il sacco");
 												//obbiettivo 5: assassino
 												theIntent.putExtra("score", 5);
 												startActivity(theIntent);
 												avanti.setVisibility(View.INVISIBLE);
-												second.setText("Parti per l'adestramento!");
+												second.setText("Parti per l'addestramento!");
 												second.setOnClickListener(new View.OnClickListener() {
 													
 													@Override
